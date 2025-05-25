5267f4b365cd7e6ad585bd29dd42b7f13fd7aa6a
Mawuli-Kplim Asase <masase@c1r11s6.42lausanne.ch>
2025-05-25 15:59:45 +0200

    ex 00 OK

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 12:58:40 by masase            #+#    #+#             */
+/*   Updated: 2025/05/25 14:23:09 by masase           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int main(int ac, char **av)
+{
+    int i = 1;
+    int j;
+
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n"; 
+    }
+    else
+    {
+        while(av[i])
+        {
+            j = 0;
+            while (av[i][j])
+            {
+                if (isalpha(av[i][j]))
+                {
+                    av[i][j] = toupper(av[i][j]);
+                }
+                j++;
+            }
+            std::cout << av[i];
+            i++;
+        }
+        std::cout << "\n";
+    }
+    return (0);
+}
---
+++ ex01/Contact.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/25 14:37:31 by masase            #+#    #+#             */
+/*   Updated: 2025/05/25 15:52:17 by masase           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.hpp"
+
+class Contact
+{
+    private:
+        char *first_name;
+        char *last_name;
+        char *phone_number;
+        char *dark_secret;
+    public:
+        //fucntion
+};
