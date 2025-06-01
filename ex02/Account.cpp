ae3251c3c4e7c2e5f059dd12a0df5fb96669adff
Maw <mawuli.asase@gmail.com>
2025-06-01 12:23:58 +0200

    ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 23:18:26 by maw               #+#    #+#             */
+/*   Updated: 2025/05/31 00:32:57 by maw              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+int Account::getNbAccounts(void)
+{
+	
+}
+int Account::getTotalAmount(void)
+{
+
+}
+int	Account::getNbDeposits(void)
+{
+
+}
+int	Account::getNbWithdrawals(void)
+{
+
+}
+void	Account::displayAccountsInfos(void)
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+int		Account::checkAmount( void ) const
+{
+
+}
+void	Account::displayStatus( void ) const
+{
+	
+}
+
+Account( int initial_deposit )
+{
+
+}
+~Account( void )
+{
+	
+}
